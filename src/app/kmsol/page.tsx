e5e8ceba55f3fcb3d2ab409c0480894e4fb6839c
Nonthasak Laoluerat <nonthasak.l@gmail.com>
2024-12-16 00:11:30 +0700

    impl: completed-with-comingsoon

--- src/app/kmsol/page.tsx
+++
@@ -1,5 +0,0 @@
-import KmsolFeature from '@/components/kmsol/kmsol-feature'
-
-export default function Page() {
-  return <KmsolFeature />
-}
---
+++ src/app/media/page.tsx
@@ -0,0 +1,6 @@
+import ComingSoon from "@/components/ui/coming-soon";
+import React from "react";
+
+export default function MediaPage() {
+  return <ComingSoon />;
+}
